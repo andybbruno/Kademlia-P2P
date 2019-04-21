c49d9a0e568a31ba1b50a3e37e36178ef5144c87
Andrea <andybbruno@gmail.com>
2019-04-21 18:47:30 +0200

    3 Nested cycles for complete analysis

--- src/Start/Start.java
+++ src/Start/Start.java
@@ -1,5 +1,6 @@
 package Start;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.io.File;
 import java.io.PrintWriter;
@@ -12,70 +13,75 @@ import Network.Node;
  */
 public class Start {
 	public static boolean SHA1 = false;
-	public static int bit = 32;
-	public static int num_nodes = 10;
-	public static int bucket_size = 2;
+	public static int bit;
+	public static int num_nodes;
+	public static int bucket_size;
 	public static int alpha = 3;
-	public static String filename = "ANALYSIS/Kad_N_" + num_nodes + "_BIT_" + bit + "_K_" + bucket_size + ".csv";
+	public static String filename;
+
+	static int[] arr_bit = { 32, 64, 128, 160, 256, 512, 1024, 2048 };
+	static int[] arr_nod = { 50, 100, 200, 500, 1000, 2000, 5000, 10000};
+	static int[] arr_buck = { 1, 2, 3, 5, 8, 13, 15, 20 };
 
 	public static void main(String[] args) {
-		
-		
-		
-//		new Analysis();
-		
-		
-		// Create the network
-		Kademlia kad = new Kademlia();
 
-		// Create the nodes
-		LinkedList<Node> nodes = new LinkedList<Node>();
+		for (int b : arr_bit) {
+			bit = b;
+			for (int nod : arr_nod) {
+				num_nodes = nod;
+				for (int buck : arr_buck) {
+					bucket_size = buck;
 
-		LinkedList<String> edges = new LinkedList<String>();
+					filename = "ANALYSIS/Kad_N_" + num_nodes + "_BIT_" + bit + "_K_" + bucket_size + ".csv";
+					System.out.println(filename);
 
-		long startTime = System.nanoTime();
+					// Create the network
+					Kademlia kad = new Kademlia();
 
-		for (int i = 0; i < num_nodes; i++) {
-			nodes.add(new Node(kad));
-			if ((i % 1000) == 0) {
-				System.out.println(i);
+					// Create the nodes
+					LinkedList<Node> nodes = new LinkedList<Node>();
+
+					// Create the edges
+					HashSet<String> edges = new HashSet<String>();
+
+					long startTime = System.nanoTime();
+
+					for (int i = 0; i < num_nodes; i++) {
+						nodes.add(new Node(kad));
+					}
+
+					for (int i = 0; i < num_nodes; i++) {
+						edges.addAll(nodes.get(i).getEdges());
+					}
+
+					int num_edges = 0;
+
+					try (PrintWriter writer = new PrintWriter(new File(filename))) {
+
+						StringBuilder sb = new StringBuilder();
+
+						for (String x : edges) {
+							sb.append(x);
+							sb.append('\n');
+							num_edges++;
+
+						}
+
+						writer.write(sb.toString());
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+					long endTime = System.nanoTime();
+
+					// get difference of two nanoTime values
+					long timeElapsed = endTime - startTime;
+
+					System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
+					System.out.println(num_edges + " edges");
+				}
 			}
 		}
-
-		for (int i = 0; i < num_nodes; i++) {
-			edges.addAll(nodes.get(i).getEdges());
-			if ((i % 1000) == 0) {
-				System.out.println(i);
-			}
-		}
-
-		int num_edges = 0;
-
-		try (PrintWriter writer = new PrintWriter(new File(filename))) {
-
-			StringBuilder sb = new StringBuilder();
-
-			for (String x : edges) {
-				sb.append(x);
-				sb.append('\n');
-				num_edges++;
-
-			}
-
-			writer.write(sb.toString());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		long endTime = System.nanoTime();
-
-		// get difference of two nanoTime values
-		long timeElapsed = endTime - startTime;
-
-		System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
-		System.out.println(num_edges + " edges");
-
-		
 	}
 }
